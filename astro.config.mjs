9a1c04e4549c5882a168e2883bcf449332889c3e
Sijo Sam <sijosam1905@gmal.com>
2024-12-06 21:34:00 +0530

    feature:  added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://sijosam.in',
+
     integrations: [
         mdx(),
         sitemap(),
         tailwind({
             applyBaseStyles: false
         })
-    ]
+    ],
+
+    adapter: vercel()
 });
