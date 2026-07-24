146d916d7e05ed210a71a354cf036e87e54e1416
mrSamDev <sijosam1905@gmail.com>
2026-07-24 10:19:19 +0530

    Update package.json to include onlyBuiltDependencies for pnpm and remove pnpm-workspace.yaml

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -15,6 +15,7 @@ export type Hero = {
     text?: string;
     image?: Image;
     actions?: Link[];
+    subtitle?: string;
 };
 
 export type Subscribe = {
@@ -105,6 +106,7 @@ const siteConfig: SiteConfig = {
     hero: {
         title: 'Sijo Sam',
         text: 'നദിക്ക് വള്ളവും മനുഷ്യർക്ക് സ്വപ്നവും ഒരുപോലെയാണ്.',
+        subtitle: 'A boat carries us across a river; a dream carries us through life.',
         image: {
             src: 'https://res.cloudinary.com/dnmuyrcd7/image/upload/f_auto,q_auto/iotd6l0if1gixlitydpq',
             alt: 'Sijo Sam - Technical Lead'
